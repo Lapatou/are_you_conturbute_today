462d25ec0f54c28bd92073a5513d8366b37671d9
Kaan Enes Çiftçi <kaanenesciftci@gmail.com>
2022-06-29 12:12:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# are_you_conturbute_today
+
+Home Screen widget that look red what when you not contributed on github today
+
---
+++ android/app/src/main/java/com/lapatou/are_you_conturbute_today/MainActivity.java
@@ -0,0 +1,6 @@
+package com.lapatou.are_you_conturbute_today;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
